6657460ce417c9509060192d2da84df5c3a41f1b
joyjoylili <zhengjoyli@outlook.com>
2025-09-26 14:10:54 +0200

    mostly done
    just left out most debugging excercises

--- session-03-automation/lotr_project/scripts/Makefile.R
+++ session-03-automation/lotr_project/scripts/Makefile.R
@@ -1,9 +1,9 @@
 ## clean out any previous work
 outputs <- c("./lotr_project/data/raw/lotr_raw.tsv",
              "./lotr_project/data/processed/lotr_clean.tsv",
-             "./lotr_project/data/processed/words_summary.tsv",
+             "./lotr_project/outputs/tables/words_summary.tsv",
              list.files(path =  "./lotr_project/outputs/figures/",pattern = "*.png$", full.names = TRUE))
-getwd()
+file.remove(outputs)
 
 ## run scripts
 source("./lotr_project/scripts/00-packages.R")
